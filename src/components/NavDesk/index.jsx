8220d5b9daf4c00a2cc46eb42f4aa7832a00118e
vinisooo <viniciuslira9397@gmail.com>
2023-01-31 20:22:48 -0400

    feat: adds animation to desktop nav

--- src/components/NavDesk/index.jsx
+++ src/components/NavDesk/index.jsx
@@ -18,34 +18,34 @@ const NavDesk = () => {
             </button>
             
             <ul>
-                <li>
+                <li data-aos="fade-right">
                     <a href="#header">01</a>
                     <span>Início</span>
                 </li>
-                <li>
+                <li data-aos="fade-right" data-aos-delay="100">
                     <a href="#about-me">02</a>
                     <span>Sobre mim</span>
                 </li>
-                <li>
+                <li data-aos="fade-right" data-aos-delay="200">
                     <a href="#technologies">03</a>
                     <span>Tecnologias</span>
                 </li>
-                <li>
+                <li data-aos="fade-right" data-aos-delay="300">
                     <a href="#projects">04</a>
                     <span>Projetos</span>
                 </li>
-                <li>
+                <li data-aos="fade-right" data-aos-delay="400">
                     <a href="#contact">05</a>
                     <span>Contato</span>
                 </li>
             </ul>
-            <footer>
+            <footer data-aos="fade-up">
                 <a href="https://github.com/vinisooo" target="_blank">
-                    <img src={github} alt="" />
+                    <img src={github}/>
                 </a>
                 
                 <a href="https://www.linkedin.com/in/vin%C3%ADcius-lira-a6294b244/" target="_blank">
-                    <img src={linkedin} alt="" />
+                    <img src={linkedin} />
                 </a>
 
             </footer>
