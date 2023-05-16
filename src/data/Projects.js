a9e457837d1b99cc4b5b7e8cf15b737b1d71a41b
vinisooo <cviniciusliramdev@gmail.com>
2023-05-16 20:22:41 -0300

    fix: adjust projects order

--- src/data/Projects.js
+++ src/data/Projects.js
@@ -1,38 +1,10 @@
 export const projects = [
     {
-        gif: "https://imageup.me/images/b2c9e395-1a09-4ad4-9bfa-20191d47eb87.gif",
-        name: "Random Chooser",
-        repo: "https://github.com/vinisooo/Random-Chooser",
-        access: "https://vinisooo.github.io/Random-Chooser/",
-        description: "Aplicação de escolha aleatória. Feito com HTML, css e Javascript"
-    },
-    {
-        gif: "https://imageup.me/images/85562916-11d2-404a-b746-5eab4bd616f0.gif",
-        name: "Toby Shop",
-        repo: "https://github.com/vinisooo/toby-shop",
-        access: "https://vinisooo.github.io/toby-shop/",
-        description: "E-commerce de roupas. Feito com HTML, css e Javascript"
-    },
-    {
-        gif: "https://imageup.me/images/23e64e13-4f09-4277-80e8-8491021aa80b.gif",
-        name: "Control Finance",
-        repo: "https://github.com/vinisooo/Control-Finance",
-        access: "https://vinisooo.github.io/Control-Finance/",
-        description: "Aplicação de controle financeiro. Feito com HTML, css e Javascript"
-    },
-    {
-        gif: "https://imageup.me/images/ff3d1a22-1a29-47d1-800a-1a8112c96d48.gif",
-        name: "Git Search",
-        repo: "https://github.com/vinisooo/git-search",
-        access: "https://vinisooo.github.io/git-search/",
-        description: "\"Pesquisador\" de perfis do github. Feito com HTML, CSS, Javascript e RestAPI"
-    },
-    {
-        gif: "https://imageup.me/images/63fc3c0f-de44-48eb-8242-accf0633d241.gif",
-        name: "Nu Kenzie",
-        repo : "https://github.com/vinisooo/nu-kenzie",
-        access: "https://react-entrega-s1-template-nu-kenzie-iyfdmnv4d-vinisooo.vercel.app/",
-        description: "Aplicação de controle financeiro. Feito com React.JS e CSS"
+        gif: "https://i.postimg.cc/Z5LQ5F3z/league-of-match.gif",
+        name: "League of Match",
+        repo: "https://github.com/vinisooo/league-of-match",
+        access: "https://lom-front-o8krp7x3c-vinisooo.vercel.app/",
+        description: "Finder de duo para league of legends realizado em grupo. Feito com React, Typescript, Styled-Components, React-Router-DOM, React-Hook-Form, yup , Context API, Python e Django"
     },
     {
         gif: "https://imageup.me/images/3de38598-342d-4bda-98fa-46712a379c3d.gif",
@@ -42,17 +14,45 @@ export const projects = [
         description: "Hamburgueria. Feita com React, Typescript, Styled-Components, React-Router-DOM, React-Hook-Form, yup , Context API e REST API"
     },
     {
-        gif: "https://i.postimg.cc/Z5LQ5F3z/league-of-match.gif",
-        name: "League of Match",
-        repo: "https://github.com/vinisooo/league-of-match",
-        access: "https://lom-front-o8krp7x3c-vinisooo.vercel.app/",
-        description: "Finder de duo para league of legends realizado em grupo. Feito com React, Typescript, Styled-Components, Fake API, React-Router-DOM, React-Hook-Form, yup , Context API e REST API"
-    },
-    {
         gif: "https://imageup.me/images/2c846c43-3e96-4bbc-abaa-60494775a714.png",
-        name: "Talk-GPT [Em desenvolvimento]",
+        name: "Talk-GPT",
         repo: "https://github.com/vinisooo/Chat-GPT",
         access: "https://chat-gpt-omega-plum.vercel.app",
-        description: "Bot Chat utilizando Inteligência artifical GPT-3, da OpenAI. Feito com React, Typescript, Styled-Components, Rest API e Context API"
+        description: "Bot Chat utilizando Inteligência artifical GPT-3 da OpenAI. Feito com React, Typescript, Styled-Components, Rest API e Context API"
+    },
+    {
+        gif: "https://imageup.me/images/63fc3c0f-de44-48eb-8242-accf0633d241.gif",
+        name: "Nu Kenzie",
+        repo : "https://github.com/vinisooo/nu-kenzie",
+        access: "https://react-entrega-s1-template-nu-kenzie-iyfdmnv4d-vinisooo.vercel.app/",
+        description: "Aplicação de controle financeiro. Feito com React.JS e CSS"
+    },
+    {
+        gif: "https://imageup.me/images/ff3d1a22-1a29-47d1-800a-1a8112c96d48.gif",
+        name: "Git Search",
+        repo: "https://github.com/vinisooo/git-search",
+        access: "https://vinisooo.github.io/git-search/",
+        description: "\"Pesquisador\" de perfis do github. Feito com HTML, CSS, Javascript e RestAPI"
+    },
+    {
+        gif: "https://imageup.me/images/b2c9e395-1a09-4ad4-9bfa-20191d47eb87.gif",
+        name: "Random Chooser",
+        repo: "https://github.com/vinisooo/Random-Chooser",
+        access: "https://vinisooo.github.io/Random-Chooser/",
+        description: "Aplicação de escolha aleatória. Feito com HTML, css e Javascript"
+    },
+    {
+        gif: "https://imageup.me/images/23e64e13-4f09-4277-80e8-8491021aa80b.gif",
+        name: "Control Finance",
+        repo: "https://github.com/vinisooo/Control-Finance",
+        access: "https://vinisooo.github.io/Control-Finance/",
+        description: "Aplicação de controle financeiro. Feito com HTML, css e Javascript"
+    },
+    {
+        gif: "https://imageup.me/images/85562916-11d2-404a-b746-5eab4bd616f0.gif",
+        name: "Toby Shop",
+        repo: "https://github.com/vinisooo/toby-shop",
+        access: "https://vinisooo.github.io/toby-shop/",
+        description: "E-commerce de roupas. Feito com HTML, css e Javascript"
     },
 ]
